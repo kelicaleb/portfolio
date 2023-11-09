a9afa13805ac658a6fdbe16abe615c344d1c5b49
Caleb Kavindu Keli <kelicaleb7@gmail.com>
2023-11-09 23:30:18 +0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Profile from './components/Profile.jsx'
-import About from './components/About.jsx'
-import Contact  from './components/Contact.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
       <App/>
-    </BrowserRouter>
   </React.StrictMode>,
 )
