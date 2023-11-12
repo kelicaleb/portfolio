11cb7ea69eaa6ce86a6b53fdf75d3dfb669c56dd
Caleb Kavindu Keli <kelicaleb7@gmail.com>
2023-11-12 13:37:27 +0300

    Deploy

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -24,20 +24,20 @@ const spanStyle = {
   const slideImages = [
     {
       url: `${Comp}`,
-      caption: 'Slide 1',
+      caption: 'SeeMore?!',
       title:"Independent Project",
       link:"https://kelicaleb.github.io/independent-project/"
     },
     {
       url: `${Old}`,
-      caption: 'Slide 2',
+      caption: 'SeeMore?!',
       title:"Old portfolio",
       link:"https://kelicaleb.github.io/caleb/"
 
     },
     {
       url: `${Adventure}`,
-      caption: 'Slide 3',
+      caption: 'Adventure',
       title:"Can be anything",
       link:"https://kelicaleb.github.io/kelicaleb/"
 
@@ -56,8 +56,8 @@ const spanStyle = {
                 {slideImage.title}
              </h1>
              </div>
-             <div className="relative flex items-center justify-center h-screen shadow-lg " key={index}>
-                     <div className=" h-[40rem]  w-[30rem]  bg-no-repeat relative flex  items-center justify-center  pt-3 shadow-lg shadow-purple-400/100" style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+             <div className="relative flex items-center justify-center h-screen shadow-md pl-8 pr-8 " key={index}>
+                     <div className=" h-[40rem]  pt-60  w-[40rem]  bg-no-repeat relative flex  pb-60 items-center justify-center   shadow-lg shadow-purple-400/100" style={{ 'backgroundImage': `url(${slideImage.url})` }}>
                          <a className="text-blue-600 font-mono text-7xl hover:text-cyan-400 " href={slideImage.link}>{slideImage.caption}</a>
                      </div>
                  </div></>
